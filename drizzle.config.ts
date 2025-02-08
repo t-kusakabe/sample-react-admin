ca2cc36fea9c47242cf38b45f7e1ca2520ce94e1
t-kusakabe <ttmac0704@gmail.com>
2025-02-09 00:09:08 +0900

    Create drizzle config

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'drizzle-kit';
+
+export default {
+  schema: './src/db/schema.ts',
+  out: './drizzle/migrations',
+  dialect: 'mysql',
+  dbCredentials: {
+    host: 'localhsot',
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: 'sample_react_admin_dev'
+  }
+} satisfies Config;
