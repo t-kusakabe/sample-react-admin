846db2ffc18678831f5a2c0a45995287d7b2680a
t-kusakabe <ttmac0704@gmail.com>
2025-02-10 16:37:03 +0900

    dbの指定

--- src/db/db.ts
+++ src/db/db.ts
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: 'sample_react_admin_dev'
 });
 
 export const db = drizzle(pool);
